c8ca01bc8f26687b169cf0998fe61e615acb9bcf
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-02-07 23:47:58 +0000

    Update astrofunctions.js

--- astrofunctions.js
+++ astrofunctions.js
@@ -1,3 +1,10 @@
 function printRADec(){
-  document.getElementById('display').innerHTML = document.getElementById("RA").value;
+  var ra_in = document.getElementById("RA").value;
+  var dec_in = document.getElementById("RA").value;
+  
+  if (your_string.indexOf(':') > -1)
+  {
+    document.getElementById('display').innerHTML = 'There is a : here!';
+  }
+ 
 }
