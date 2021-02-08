494293d02b5c90dd769687c9069fa045d3321289
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-02-08 00:40:33 +0000

    Update astrofunctions.js

--- astrofunctions.js
+++ astrofunctions.js
@@ -7,8 +7,8 @@ function printRADec(){
     var ra_split = ra_in.split(":");
     var dec_split = dec_in.split(":");
     
-    var ra_out = ((360 / 24) * parseFloat(ra_split.slice(0, 1))) + ((360 / 24) * (parseFloat(ra_split.slice(0, 2)) / 60)) + ((360 / 24) * (parseFloat(ra_split.slice(0, 3)) / 3600));
-    var dec_out = parseFloat(dec_split.slice(0, 1)) + ((360 / 24) * (parseFloat(dec_split.slice(0, 2)) / 60)) + ((360 / 24) * (parseFloat(dec_split.slice(0, 3)) / 3600));
+    var ra_out = ((360.0 / 24.0) * parseFloat(ra_split.slice(0, 1))) + ((360.0 / 24.0) * (parseFloat(ra_split.slice(0, 2)) / 60.0)) + ((360.0 / 24.0) * (parseFloat(ra_split.slice(0, 3)) / 3600.0));
+    var dec_out = parseFloat(dec_split.slice(0, 1)) + ((360.0 / 24.0) * (parseFloat(dec_split.slice(0, 2)) / 60.0)) + ((360.0 / 24.0) * (parseFloat(dec_split.slice(0, 3)) / 3600.0));
     document.getElementById('display').innerHTML = 'RA: ' + ra_out.toString() + ', Dec: ' + dec_out.toString();
   }
  
