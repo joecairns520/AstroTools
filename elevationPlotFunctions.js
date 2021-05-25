f934202f113958ce91658c71f0c67004fefce81a
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-05-25 19:00:18 +0100

    Update elevationPlotFunctions.js

--- elevationPlotFunctions.js
+++ elevationPlotFunctions.js
@@ -86,6 +86,8 @@ var UT_time = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200,
 // For drawing the lines
 // var elevation = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
 var elevation = elevationFromObserver(50.0, 50.0, 50.0, 50.0, 07042021)
+// Output the result into the display 
+document.getElementById('display').innerHTML = elevation;
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
