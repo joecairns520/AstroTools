dcc700d0d264069c10562d1abe0854e2969c009a
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-05-25 18:46:10 +0100

    Update elevationPlotFunctions.js

--- elevationPlotFunctions.js
+++ elevationPlotFunctions.js
@@ -37,11 +37,11 @@ function returnElevation(Dec, HA, lat) {
 // the date parameter must be in ddmmyyyy format.
 function elevationFromObserver(RA, Dec, long, lat, date){
    // Take RA and Dec from the input fields
-   //   var RA = parseFloat(document.getElementById("RA").value);
-   //   var Dec = parseFloat(document.getElementById("Dec").value);
-   //   var long = parseFloat(document.getElementById("long").value);
-   //   var lat = parseFloat(document.getElementById("lat").value);
-   //   var date = document.getElementById("date").value;
+     var RA = parseFloat(document.getElementById("RA").value);
+     var Dec = parseFloat(document.getElementById("Dec").value);
+     var long = parseFloat(document.getElementById("long").value);
+     var lat = parseFloat(document.getElementById("lat").value);
+     var date = document.getElementById("date").value;
 
    // We're going to want the UT to span the whole day for the plot 
    var UT_time = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300];
@@ -84,8 +84,8 @@ function elevationFromObserver(RA, Dec, long, lat, date){
 // Our labels along the x-axis
 var UT_time = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300];
 // For drawing the lines
-var elevation = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
-// elevationFromObserver(50.0, 50.0, 50.0, 50.0, 07042021)
+// var elevation = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+var elevation = elevationFromObserver(50.0, 50.0, 50.0, 50.0, 07042021)
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
