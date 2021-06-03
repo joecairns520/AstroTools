84bfa008ef067262e27d9965c330a72b80e1d178
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-06-03 14:27:39 +0100

    Update elevationPlotFunctions.js

--- elevationPlotFunctions.js
+++ elevationPlotFunctions.js
@@ -118,7 +118,7 @@ var myChart = new Chart(ctx, {
         data: elevation,
         label: "Target",
         borderColor: "#3e95cd",
-	color = "#FFFFFF",
+	color: "#FFFFFF",
         fill: false
       }
     ]
