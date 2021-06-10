7bfea90cca1fd0f563d17cecfcb3203f1474b01f
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-06-10 15:29:52 +0100

    Update elevationPlotFunctions.js

--- elevationPlotFunctions.js
+++ elevationPlotFunctions.js
@@ -112,7 +112,7 @@ var UT_time = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 // For drawing the lines
 // var elevation = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
 var elevation = elevationFromObserver(333.75, -11.8, 0.1278, 51.5, '04062021');
-
+document.getElementById("location").innerHTML = elevation;
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: 'line',
