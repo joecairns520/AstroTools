4a89aee4f7d32600d2cc22131d16b543c21222e2
joecairns520 <77118264+joecairns520@users.noreply.github.com>
2021-09-23 12:05:22 +0100

    Update elevationPlotFunctions.js

--- elevationPlotFunctions.js
+++ elevationPlotFunctions.js
@@ -136,7 +136,7 @@ function makePlot() {
 	// Determine the longitude and latitude we'll need based on the selected location
 	var location = document.getElementById("location").innerHTML.slice(19, );
 	var longLat = returnLongLat(location);
-	window.alert(location);
+	window.alert(UT_time);
 	
 // 	// Get the RA and Dec
 // 	var ra = parseFloat(document.getElementById("RA").value);
